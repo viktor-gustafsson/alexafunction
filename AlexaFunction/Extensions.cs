1b8c7dbe0865871cd76a59e89c11118cfd294df4
Viktor Gustafsson <viktor.b.gustafsson@gmail.com>
2019-12-18 20:59:39 +0100

    fixes requst type check

--- AlexaFunction/Extensions.cs
+++ AlexaFunction/Extensions.cs
@@ -6,9 +6,9 @@ namespace AlexaFunction
     public static class Extensions
     {
         public static bool IsLaunchRequest(this SkillRequest skillRequest) =>
-            skillRequest.GetType() == typeof(LaunchRequest);
+            skillRequest.GetRequestType() == typeof(LaunchRequest);
 
         public static bool IsIntentRequest(this SkillRequest skillRequest) =>
-            skillRequest.GetType() == typeof(IntentRequest);
+            skillRequest.GetRequestType() == typeof(IntentRequest);
     }
 }
